6f964c547a43e4005c1cb2181e8bd7a2ad976196
javaberger <java1berg@gmail.com>
2016-04-29 12:14:51 +0300

    I-Shop

--- I-Shop/I-Shop/Controllers/HomeController.cs
+++ I-Shop/I-Shop/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace I_Shop.Controllers
@@ -10,20 +11,6 @@ namespace I_Shop.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
