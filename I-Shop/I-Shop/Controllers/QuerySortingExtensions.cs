e9d1b79517a41d276fc60740091519c1b0a214b8
javaberger <java1berg@gmail.com>
2016-05-14 17:07:45 +0300

    update

---
+++ I-Shop/I-Shop/Controllers/QuerySortingExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+
+
+namespace I_Shop.Controllers
+{
+    public static class QuerySortingExtensions
+    {
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> query, IEnumerable<string> sort) where T : class
+        {
+            if (sort != null)
+            {
+                List<string> sortFields = new List<string>();
+
+                foreach (string sortField in sort)
+                {
+                    if (sortField.StartsWith("+"))
+                    {
+                        sortFields.Add(string.Format("{0} ASC", sortField.TrimStart('+')));
+                    }
+                    else if (sortField.StartsWith("-"))
+                    {
+                        sortFields.Add(string.Format("{0} DESC", sortField.TrimStart('-')));
+                    }
+                    else
+                    {
+                        sortFields.Add(sortField);
+                    }
+                }
+
+                return query.OrderBy(String.Join(",", sortFields));
+            }
+
+            return query;
+        }
+    }
+}
