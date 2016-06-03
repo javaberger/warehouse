062430a91aeb50a3a8124db07b82e83fc3e53786
javaberger <java1berg@gmail.com>
2016-06-03 15:52:07 +0300

    filesharing

---
+++ FileShare/FileShare/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FileShare
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "MyMap",
+              url: "{id}",
+              defaults: new { controller = "Home", action = "Storage", id = UrlParameter.Optional },
+              constraints: new { id = "\\d+" }
+          );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ FileShare/FileShare/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileShare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Random MD5 Key
+        public static string GenerateMD5Uniq()
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(Guid.NewGuid().ToString());
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+
+        public ActionResult Storage(string id)
+        {
+            return PartialView("_storage");
+        }
+
+
+        public ActionResult Create()
+        {
+            ViewBag.HashCode = GenerateMD5Uniq();
+            return View("add_storage");
+        }
+    }
+}
