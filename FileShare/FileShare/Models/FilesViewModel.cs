532d5fa06bb51a48af4a5d9bf58cb831191cd5e7
javaberger <java1berg@gmail.com>
2016-08-25 17:47:55 +0300

    FileSharing

---
+++ FileShare/FileShare/Models/FilesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FileShare.Helpers;
+using FileShare.Models;
+
+namespace FileShare.Helpers
+{
+        public class FilesViewModel
+        {
+            public ViewDataUploadFilesResult[] Files { get; set; }
+        }
+}
